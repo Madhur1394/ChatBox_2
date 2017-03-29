826775fa7173810cc6e359b08ef6eaf1b14605dc
dell <madhur.jain1394@gmail.com>
2017-03-29 11:00:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/myapplication3/MainActivity.java
@@ -0,0 +1,214 @@
+package com.example.dell.myapplication3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.CharacterPickerDialog;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener childEventListener;
+
+    private EditText editText_message;
+    private MessageAdapter messageAdapter;
+    private ProgressBar progressBar;
+    private ListView listView_message;
+
+    public String userName = "ANONYMOUS";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.chat);
+        setSupportActionBar(toolbar);
+
+        editText_message = (EditText) findViewById(R.id.editText3);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        listView_message = (ListView) findViewById(R.id.messageListView);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+           userName = extras.getString("user_name");
+        }
+        Toast.makeText(getApplicationContext(),userName,Toast.LENGTH_LONG).show();
+
+
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton floatingActionButton_att = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+
+        // Initialize message ListView and its adapter
+        List<ChatMessage> chatMessages = new ArrayList<>();
+        messageAdapter = new MessageAdapter(this, R.layout.item_message, chatMessages);
+        listView_message.setAdapter(messageAdapter);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        fab.setEnabled(false);
+        //For message sending
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //For Save Messgaes
+                ChatMessage chatMessage = new ChatMessage(editText_message.getText().toString().trim(),userName,null);
+
+
+                mDatabaseReference.push().setValue(chatMessage);
+
+                editText_message.setText("");
+            }
+        });
+
+        //For Attachment PickUp
+        floatingActionButton_att.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //For message write
+        editText_message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() > 0) {
+                    fab.setEnabled(true);
+                } else {
+                    fab.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+
+
+
+        });
+
+        //Authentication code.
+        //Get Firebase auth Instance
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //User signed in
+                    Toast.makeText(getApplicationContext(),"User is signed in",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    //User Signed Out
+                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        //Databse Code
+        //Get Databse instance
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("messages");
+
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                ChatMessage chatMessage = dataSnapshot.getValue(ChatMessage.class);
+                messageAdapter.add(chatMessage);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        mDatabaseReference.addChildEventListener(childEventListener);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            mAuth.signOut();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
